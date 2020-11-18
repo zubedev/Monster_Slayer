b4c0a4b9a549e6270a9fb72a8e557bd585939049
Zubair Beg <ziibii88@gmail.com>
2020-11-18 16:48:53 +0800

    added special attack action

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function getAttackVal(min, max) {
+function getRandomValue(min, max) {
     // Returns a random attack integer value between min and max args
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -8,17 +8,33 @@ const app = Vue.createApp({
         return {
             healthPlayer: 100,      // starting health of the Player
             healthMonster: 100,     // starting health of the Monster
+            currentRound: 0         // round obviously starts at 0
+        }
+    },
+    computed: {
+        healthMonsterStyle() { return { width: this.healthMonster + '%' } },
+        healthPlayerStyle() { return { width: this.healthPlayer + '%' } },
+        disableSpecialAttack() {
+            // allow to use SpecialAttack every 3 rounds
+            return this.currentRound % 3 !== 0 || this.currentRound === 0
         }
     },
     methods: {
         attackByMonster() {
-            const attackVal = getAttackVal(8, 15)
+            const attackVal = getRandomValue(8, 15)
             this.healthPlayer -= attackVal
         },
         attackByPlayer() {
-            const attackVal = getAttackVal(5, 12)
+            this.currentRound++ // increment the round count
+            const attackVal = getRandomValue(5, 12)
             this.healthMonster -= attackVal
             this.attackByMonster() // player gets hit by monster on each attack
+        },
+        specialAttack() {
+            this.currentRound++ // increment the round count
+            const attackVal = getRandomValue(10, 25) // larger value than normal
+            this.healthMonster -= attackVal
+            this.attackByMonster() // player should still get hit by monster
         }
     }
 });
